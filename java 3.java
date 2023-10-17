3e481ff7e7500f2356658caa86e409819a72d09c
RoshithaBandaru <147608586+RoshithaBandaru@users.noreply.github.com>
2023-10-17 15:42:48 +0530

    corrected code

--- java 3.java
+++ java 3.java
@@ -1,29 +1,29 @@
 //3. Check if the input is panagram or not. (Panagram is a sentence that contains all the
 alphabets from a-z)
 
-import java.util.HashSet;
-import java.util.Set;
-
-public class pangram {
-    public static void main(String[] args) {
-        String input = System.console().readLine("Enter a string: ");
-
-        Set<Character> uniqueCharacters = new HashSet<>();
-
-        for (int i = 0; i < input.length(); i++) {
-            char character = input.charAt(i);
-            if (Character.isLetter(character)) {
-                uniqueCharacters.add(Character.toLowerCase(character));
-            }
-        }
-
-        boolean isPangram = uniqueCharacters.size() == 26;
-
-        // Print 
-        if (isPangram) {
-            System.out.println("The input string is a pangram!");
-        } else {
-            System.out.println("The input string is not a pangram.");
-        }
-    }
+public class Main
+{
+	public static void main(String[] args) {
+	String str = "write any panagram statement";
+      boolean[] alphaList = new boolean[26];
+      int index = 0;
+      int flag = 1;
+      for (int i = 0; i < str.length(); i++) {
+         if ( str.charAt(i) >= 'A' && str.charAt(i) <= 'Z') {
+            index = str.charAt(i) - 'A';
+         }else if( str.charAt(i) >= 'a' && str.charAt(i) <= 'z') {
+            index = str.charAt(i) - 'a';
+      }
+      alphaList[index] = true;
+   }
+   for (int i = 0; i <= 25; i++) {
+      if (alphaList[i] == false)
+      flag = 0;
+   }
+   System.out.print("String: " + str);
+   if (flag == 1)
+      System.out.print("The above string is a pangram.");
+   else
+      System.out.print("The above string is not a pangram.");
+   }
 }
